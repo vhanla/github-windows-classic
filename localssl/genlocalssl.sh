b28bb7a2cf82bb8e8900e45c6a961ef46cd68980
vhanla <vhanla@gmail.com>
2018-06-06 06:34:24 -0500

    First commit

---
+++ localssl/genlocalssl.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Generate local certificates for development https://www.akadia.com/services/ssh_test_certificate.htmlting local ssl,  
+
+openssl genrsa -des3 -out server.key 1024
+openssl req -new -key server.key -out server.csr
+cp server.key server.key.org
+openssl rsa -in server.key.org -out server.key
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+
